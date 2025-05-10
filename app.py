fd9332fdc8b21a80c1a87a67062b11888a4dcc1d
Trinh Nguyen <nguyentrinh150221@gmail.com>
2025-05-10 13:50:39 +0700

    update

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ def add_date_column(df, date_str):
 def load_and_clean_reports(br_file, ads_file, date_str):
     br = pd.read_csv(br_file)[['(Parent) ASIN', '(Child) ASIN', 'Sessions - Total', 'Units Ordered']]
     br.columns = ['Parent_ASIN', 'Child_ASIN', 'Sessions', 'Units_Ordered']
+    br['Sessions'] = br['Sessions'].astype(str).str.replace(',', '').astype(int)
+    br['Units_Ordered'] = br['Units_Ordered'].astype(str).str.replace(',', '').astype(int)
+    
     br = add_date_column(br, date_str)
 
     ads = pd.read_csv(ads_file)[['Products', 'Clicks', 'Spend(USD)']]
@@ -69,12 +72,7 @@ if br_file and ads_file and date_input:
                 filtered_df = merged_df[(merged_df['Date'] >= pd.to_datetime(start_date)).dt.date &
                                         (merged_df['Date'] <= pd.to_datetime(end_date)).dt.date]
                 df_final = filtered_df[['Child_ASIN', 'Sessions', 'Units_Ordered', 'Clicks_Ads', 'Spend_Ads', 'Date']].sort_values(['Date', 'Child_ASIN'])
-                # Bỏ dâu phẩy trong cột Sessions và Units_Ordered
-                df_final['Sessions'] = df_final['Sessions'].astype(str).str.replace(',', '')
-                df_final['Units_Ordered'] = df_final['Units_Ordered'].astype(str).str.replace(',', '')
-                # Chuyển đổi kiểu dữ liệu về số nguyên
-                df_final['Sessions'] = pd.to_numeric(df_final['Sessions'], errors='coerce').fillna(0).astype(int)
-                df_final['Units_Ordered'] = pd.to_numeric(df_final['Units_Ordered'], errors='coerce').fillna(0).astype(int)
+
                 try:
                     export_to_gsheet(df_final, "18juLU-AmJ8GVnKdGFrBrDT_qxqxcu_aLNK-2LYOsuYk", cred_dict, "DAILY_TH", int(start_row))
                     st.success("✅ Data pushed to Google Sheets successfully!")
